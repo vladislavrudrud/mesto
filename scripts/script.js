9cf50f07978b32c4f5726ecca897399c90cd50d7
Владислав Рудь <kolosovvvskiy@yandex.ru>
2023-06-07 16:42:59 +0300

    mesto5

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,15 @@ const popup = document.querySelector('.popup');
 const edit = document.querySelector('.profile__edit-button');
 const closed = document.querySelector('.popup__closed');
 const formElement = document.querySelector('.popup__form');
-const nameinput = document.getElementById('name');
-const descriptioninput = document.getElementById('description');
+const nameInput = document.getElementById('name');
+const descriptionInput = document.getElementById('description');
 const author = document.querySelector('.profile__author');
 const description = document.querySelector('.profile__description');
 
 const popupOpen = () => {
     popup.classList.add('popup_opened');
-    nameinput.value = author.textContent;
-    descriptioninput.value = description.textContent;
+    nameInput.value = author.textContent;
+    descriptionInput.value = description.textContent;
 }
 
 const popupClosed = () => {
@@ -19,8 +19,8 @@ const popupClosed = () => {
 
 const handleFormSubmit = (evt) => {
     evt.preventDefault();
-    author.textContent = nameinput.value;
-    description.textContent = descriptioninput.value;
+    author.textContent = nameInput.value;
+    description.textContent = descriptionInput.value;
     popupClosed();
 }
 
